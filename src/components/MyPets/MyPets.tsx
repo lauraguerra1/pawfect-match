93f7574616fc5f3058741a59dd5cfca30c8dd85b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-08-03 19:24:33 -0400

    Add dialog box to delete pet

--- src/components/MyPets/MyPets.tsx
+++ src/components/MyPets/MyPets.tsx
@@ -1,22 +1,65 @@
 import './MyPets.css'
 import { Dog, Cat } from "../../types"
 import Pet from '../Pet/Pet'
+import { useState } from 'react'
+
 interface MyPetsProps {
   savedPets: (Dog | Cat)[]
   deletePet: (oldPet: Dog | Cat) => void
 }
 const MyPets = ({savedPets, deletePet}: MyPetsProps) => {
+  const [modalOpen, setModalOpen] = useState(false)
+  const [petToDelete, setPetToDelete] = useState<Cat | Dog | null>(null)
+
+  const openModal = (pet: Cat | Dog) => {
+    document.querySelector('dialog')?.showModal()
+    setModalOpen(true)
+    setPetToDelete(pet)
+  }
+
+  const closeModal = () => {
+    document.querySelector('dialog')?.close()
+    setModalOpen(false)
+    setPetToDelete(null)
+  }
+
   const savedPetEls = savedPets.map(pet => {
-    return <Pet pet={pet} key={pet.name} />
+    return (
+      <Pet 
+        pet={pet} 
+        key={pet.name} 
+        openModal={openModal}
+      />
+      )
   })
 
   return (
-    <section className='pawfect-matches'>
-      <h1>Your Pawfect Matches</h1>
-      <section className='saved-pets'>
-        {savedPetEls}
-      </section>
-    </section>
+    <>
+      <div style={{maxHeight: '90vh', overflow: 'auto'}}>
+        <section className={modalOpen ? 'blur pawfect-matches' : 'pawfect-matches'}>
+          <h1>Your Pawfect Matches</h1>
+          <section className='saved-pets'>
+            {savedPetEls}
+          </section>
+        </section>
+      </div>
+      <dialog className='dialog' >
+      <div className='dialog-flex'>
+        <p>Are you sure you want to remove the {petToDelete?.name} from your pets?</p>
+        <div>
+          <button onClick={closeModal}>CANCEL</button>
+          <button 
+            onClick={() => {
+              closeModal()
+              if(petToDelete) deletePet(petToDelete)
+            }}
+          >
+            REMOVE PET
+          </button>
+        </div>
+      </div>
+    </dialog>
+    </>
   )
 }
 
--- src/components/Pet/Pet.tsx
+++ src/components/Pet/Pet.tsx
@@ -1,13 +1,21 @@
 import './Pet.css'
 import bookmark from '../../images/bookmark.png'
 import {Dog, Cat} from '../../types'
+import { useState } from 'react'
 
-const Pet = ({pet}: {pet: Dog | Cat}) => {
+interface PetProps {
+  pet: Dog | Cat
+  openModal: (pet: Cat | Dog) => void
+}
+
+const Pet = ({pet, openModal}: PetProps) => {
+
+
   return (
     <section className='pet'>
       <img src={pet.image_link} alt={pet.name} />
       <h2>{pet.name}</h2>
-      <button className='bookmark-button'><img src={bookmark} alt='bookmark-btn'/></button>
+      <button onClick={() => openModal(pet)} className='bookmark-button'><img src={bookmark} alt='bookmark-btn'/></button>
     </section>
   )
 }
