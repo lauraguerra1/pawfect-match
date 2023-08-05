d5dc6490dd6b0724e647a918361958216ac49c53
Laura Guerra <l.garciaguerra1@gmail.com>
2023-08-05 13:43:21 -0400

    Add link to single pets details

--- src/components/Pet/Pet.tsx
+++ src/components/Pet/Pet.tsx
@@ -1,7 +1,7 @@
 import './Pet.css'
 import bookmark from '../../images/bookmark.png'
 import {Dog, Cat} from '../../types'
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 interface PetProps {
   pet: Dog | Cat
@@ -13,8 +13,8 @@ const Pet = ({pet, openModal}: PetProps) => {
 
   return (
     <section className='pet'>
-      <img src={pet.image_link} alt={pet.name} />
-      <h2>{pet.name}</h2>
+      <Link className='pet-img-link' to={`/single-pet/${pet.name.replaceAll(' ',  '-')}`}> <img src={pet.image_link} alt={pet.name} /></Link>
+      <Link className='title-link' to={`/single-pet/${pet.name.replaceAll(' ',  '-')}`}><h2>{pet.name}</h2></Link>
       <button onClick={() => openModal(pet)} className='bookmark-button'><img src={bookmark} alt='bookmark-btn'/></button>
     </section>
   )
--- src/components/PetDetails/PetDetails.tsx
+++ src/components/PetDetails/PetDetails.tsx
@@ -1,7 +1,17 @@
-const PetDetails = ({}) => {
+import { Dog, Cat } from "../../types"
+import { useParams } from "react-router-dom"
+
+interface PetDetailsProps {
+  checkIfSaved: (pet: Cat | Dog) => boolean
+}
+
+const PetDetails = ({checkIfSaved}: PetDetailsProps) => {
+  const petName = useParams().name
   return (
     <section>
 
     </section>
   )
 }
+
+export default PetDetails
